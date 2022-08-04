60aaba4946a85794e0f706eeae110bcad80474fb
usrrt <gkenfktm@gmail.com>
2022-08-04 09:56:01 +0900

    카메라 수정 및 수치조정

--- Solo Project_3D/Assets/Scripts/CamFollow.cs
+++ Solo Project_3D/Assets/Scripts/CamFollow.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CamFollow : MonoBehaviour
 {
     public Transform target;
-
+    public float yPos;
     private void Update()
     {
-        Vector3 newCamPos = new Vector3(target.position.x, GameManager.Instance.LastCheckPoint.y, transform.position.z);
+        Vector3 newCamPos = new Vector3(target.position.x, GameManager.Instance.LastCheckPoint.y - yPos, transform.position.z);
         gameObject.transform.position = newCamPos;
     }
 }
